b9cac2198fd6a5f16a16ec20844786a3febc60c3
Paras <sivssdn@gmail.com>
2016-02-18 19:11:11 +0530

    'first_commit'

---
+++ attendance.php
@@ -0,0 +1,16 @@
+<?php
+ date_default_timezone_set("Asia/Kolkata");
+ header("Content-type:application/json");
+  
+ $uid=htmlspecialchars($_GET['uid']);
+ $db=new PDO("mysql:host=localhost;dbname=ugs","root","");
+  $data=$db->prepare("SELECT `name` FROM `ashoka_ugs` WHERE `card_number` LIKE '{$uid}';");
+  $data->execute();
+  $result=$data->fetch();
+  $hours=date('H');
+  $min=date('i');
+  //$sec=date('c');
+	 print("{name:'{$result[0]}',hour:'{$hours}',min:'{$min}'}");
+  $db=null;
+
+?>
